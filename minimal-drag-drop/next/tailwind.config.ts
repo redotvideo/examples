13d649a13d37b1168571ab6c1f6f39dc2a61a903
Justus Mattern <122226645+justusmattern27@users.noreply.github.com>
2024-09-22 19:36:08 +0200

    minimal drag and drop example (#13)
    * minimal drag and drop example
    
    * remove actions from saas template
    
    * update versions

---
+++ minimal-drag-drop/next/tailwind.config.ts
@@ -0,0 +1,30 @@
+/* eslint-disable naming-convention */
+import type {Config} from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				gray: {
+					50: '#FAFAFA',
+					100: '#F3F3F3',
+					200: '#E5E5E5',
+					300: '#C7C7C7',
+					400: '#ACACAC',
+					500: '#808080',
+					600: '#5F5F5F',
+					700: '#4E4E4E',
+					800: '#373737',
+					900: '#2A2A2A',
+					950: '#151515',
+				},
+			},
+		},
+	},
+};
+export default config;
